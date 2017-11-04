736923077c2839b9754a02b9eae03718b39d2b77
Mayank Shah <mayank.24shah@gmail.com>
2017-11-04 23:56:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,9 +4,9 @@ function updateObjectWithKeyAndValue(object, key, value){
 }
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
   object[key] = value
-  return object 
+  return object
 }
 function  deleteFromObjectByKey(object, key){
-  delete object.key 
+  delete object.key
   return object 
 }
